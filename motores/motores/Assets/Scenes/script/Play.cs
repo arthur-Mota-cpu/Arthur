eb44dd5896ee7a231b59477a64e8621c2c6c3e94
arthur-Mota-cpu <arthur.mota@escolar.ifrn.edu.br>
2025-06-02 11:08:22 -0300

    AAAA

--- motores/motores/Assets/Scenes/script/Play.cs
+++ motores/motores/Assets/Scenes/script/Play.cs
@@ -1,74 +1,34 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
-public class PlayerController : MonoBehaviour
+public class Play : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 12f;
-    public float extraMoveSpeed = 0.7f; // para movimentação direta (WASD)
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.1f;
-    public LayerMask groundLayer;
-
-    private Rigidbody2D rb;
-    private Animator animator;
-    private float moveInput;
-    private bool isGrounded;
+    private int numero = 0;
+    public float velocidade = 40;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        numero = 0;
+
     }
 
     void Update()
     {
-        // Entrada de movimento horizontal (A/D ou setas)
-        moveInput = Input.GetAxisRaw("Horizontal");
+        //debug.log(numero0;
+        //numero = numero + 1;
 
-        // Flip do personagem
-        if (moveInput != 0)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(moveInput), 1f, 1f);
-        }
-
-        // Animação de corrida
-        animator.SetFloat("Speed", Mathf.Abs(moveInput));
-
-        // Verificação de chão
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        animator.SetBool("IsJumping", !isGrounded);
-
-        // Pulo
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            Debug.Log("cardique");
-        }
-
-        // Movimento adicional com W, A, S, D (movimentação direta sem física)
         if (Input.GetKey(KeyCode.A))
-            transform.position += new Vector3(-extraMoveSpeed * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(extraMoveSpeed * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0, extraMoveSpeed * Time.deltaTime, 0);
-        if (Input.GetKey(KeyCode.S))
-            transform.position += new Vector3(0, -extraMoveSpeed * Time.deltaTime, 0);
-    }
-
-    void FixedUpdate()
-    {
-        // Movimento com física
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
+            }
+        
+            if (Input.GetKey(KeyCode.Space))
+            {
+                gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
         }
     }
 }
