a473d69403e4bc67089d4ba2e4ecdff8af75184c
arthur-Mota-cpu <arthur.mota@escolar.ifrn.edu.br>
2025-06-02 11:47:30 -0300

    a
    aaaaaaaaaaaaa

--- motores/motores/Assets/Scenes/script/Play.cs
+++ motores/motores/Assets/Scenes/script/Play.cs
@@ -1,34 +1,38 @@
 using UnityEngine;
 
-public class Play : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    private int numero = 0;
+    
     public float velocidade = 40;
-
+    public float forcaDoPulo = 4;
+    
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    
     void Start()
     {
-        numero = 0;
-
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        //debug.log(numero0;
-        //numero = numero + 1;
-
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            sprite.flipX = true;
         }
-
+        
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
-            }
-        
-            if (Input.GetKey(KeyCode.Space))
-            {
-                gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            sprite.flipX = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
+        }
+
     }
 }
