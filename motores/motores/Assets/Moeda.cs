e186c935bae8c56fad79460dcb712535b5f31ecc
arthur-Mota-cpu <arthur.mota@escolar.ifrn.edu.br>
2025-09-15 07:46:51 -0300

    Continua 5

---
+++ motores/motores/Assets/Moeda.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class Chave : MonoBehaviour
+{
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
